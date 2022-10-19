9adee5dfb581df69b2553cbaad0a6649add696ff
derrickjs <85529367+derrickjs@users.noreply.github.com>
2022-10-19 13:40:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+function removeForm(){
+    var form = document.querySelector("form");
+
+    form.style.display = "none";
+}
+
+document.querySelector("#submitForm").addEventListener("click", removeForm);
+
+function addText(){
+    var show = document.querySelector(".show");
+
+    show.style.display = "block";
+}
+
+document.querySelector("#submitForm").addEventListener("click", addText);
+
+
+
+function highlighter(){
+    var highLight = document.querySelector(".date");
+
+    highLight.style.backgroundColor = "yellow";
+}
+
+document.querySelector("form").addEventListener("mouseenter", highlighter);
+
+function highlighterOff(){
+    var highLight = document.querySelector(".date");
+
+    highLight.style.backgroundColor = "white";
+}
+
+document.querySelector("form").addEventListener("mouseleave", highlighterOff );
+
+
+
+function addPicture(){
+    var rightSide = document.querySelector(".rightside");
+
+    rightSide.style.backgroundImage = "url('https://res.cloudinary.com/derrickstahlman/image/upload/v1666186868/smile_teeth_jjrzvs.jpg')";
+
+    rightSide.style.backgroundSize = "cover";
+
+}
+
+document.querySelector("#submitForm").addEventListener("click", addPicture);
+
+
+function hoverButton(){
+    document.querySelector("#submitForm").innerHTML = "Get Ready to Smile";
+
+    
+}
+
+function offHoverButton(){
+    document.querySelector("#submitForm").innerHTML = "Click to Submit";
+
+    
+}
+
+document.querySelector("#submitForm").addEventListener("mouseover", hoverButton);
+document.querySelector("#submitForm").addEventListener("mouseleave", offHoverButton);
